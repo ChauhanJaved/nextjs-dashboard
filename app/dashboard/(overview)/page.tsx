1a25008cc23130528a9a0090dd33145ac2f5c448
Javed Chauhan <chauhanjaved@gmail.com>
2023-11-25 13:30:27 +0530

    Update: fetching latest invoices in a component level in LatestInvoices component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {  fetchLatestInvoices, fetchCardData } from '../../lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 
  
@@ -36,7 +36,10 @@ export default async function Page() {
       <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
       </Suspense>
-      <LatestInvoices latestInvoices={latestInvoices} />
+      <Suspense fallback={<LatestInvoicesSkeleton />}>
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </Suspense>
+      
       </div>
     </main>
   );
