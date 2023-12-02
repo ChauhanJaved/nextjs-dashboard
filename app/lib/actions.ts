35fa560ba06df0d18e6267f6cd4e3272073f3e6e
Javed Chauhan <chauhanjaved@gmail.com>
2023-12-02 12:19:12 +0530

    Update: server action for delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -56,3 +56,7 @@ export async function createInvoice(formData: FormData) {
      redirect('/dashboard/invoices');
 }
     
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
